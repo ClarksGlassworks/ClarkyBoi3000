514f06e1e86df2f40d28f5e7b7b94d0c9eb15503
Aaron Taylor <aaron@stayapp.co>
2023-12-11 07:41:02 -0500

    Brought in React Three Fiber

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,82 @@
+'use client'
 import Image from 'next/image'
-
+import { createRoot } from 'react-dom/client'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls, Box, useCursor } from '@react-three/drei'
+import { useRef, useState } from 'react'
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between hash-lines z-10">
+
+
+    <main className="flex min-h-screen flex-col items-center justify-between">
+
+
+
+      <div className="hash-lines min-h-screen w-full h-full z-20 fixed left-0 right-0 top-0 bottom-0"></div>
       <div className="layer1 min-h-screen w-full h-full z-20 fixed left-0 right-0 top-0 bottom-0"></div>
-      <Image 
-        src="https://wp.clarksglassworks.com/wp-content/uploads/2023/12/clark-tape.png" 
-        alt="Clark Tape" 
-        width={500} 
+      <div id="canvas-container" className='w-full h-screen z-30'>
+        <Canvas shadows camera={{ fov: 75, position: [0, 0, 20] }} className='canvas1 w-full h-screen'>
+
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <Box position={[-1.2, 0, 0]} />
+          {/* <Frame id="01" name={`pick\nles`} author="Omar Faruq Tawsif" bg="#e4cdac" position={[-1.15, 0, 0]} rotation={[0, 0.5, 0]}>
+      <Gltf src="pickles_3d_version_of_hyuna_lees_illustration-transformed.glb" scale={8} position={[0, -0.7, -2]} />
+    </Frame> */}
+        </Canvas>
+      </div>
+
+      <Image
+        src="https://wp.clarksglassworks.com/wp-content/uploads/2023/12/corner-leopard.png"
+        alt="Corner Leopard"
+        width={500}
+        height={300}
+        className='z-30 fixed top-0 right-0'
+      />
+      {/* <Image
+        src="https://wp.clarksglassworks.com/wp-content/uploads/2023/12/clark-tape.png"
+        alt="Clark Tape"
+        width={500}
         height={300}
         className='z-30 relative'
       />
-      <Image 
-        src="https://wp.clarksglassworks.com/wp-content/uploads/2023/12/corner-leopard.png" 
-        alt="Corner Leopard" 
-        width={500} 
+
+      <Image
+        src="https://wp.clarksglassworks.com/wp-content/uploads/2023/12/gameboy-7.png"
+        alt="Gameboy"
+        width={500}
         height={300}
         className='z-30 relative'
-      />
-      <Image 
-        src="https://wp.clarksglassworks.com/wp-content/uploads/2023/12/gameboy-7.png" 
-        alt="Gameboy" 
-        width={500} 
-        height={300}
-        className='z-30 relative'
-      />
+      /> */}
     </main>
+  )
+}
+
+function Frame({ id, name, author, bg, width = 1, height = 1.61803398875, children, ...props }) {
+  const portal = useRef()
+  const [, setLocation] = useLocation()
+  const [, params] = useRoute('/item/:id')
+  const [hovered, hover] = useState(false)
+  useCursor(hovered)
+  useFrame((state, dt) => easing.damp(portal.current, 'blend', params?.id === id ? 1 : 0, 0.2, dt))
+  return (
+    <group {...props}>
+      <Text font={suspend(medium).default} fontSize={0.3} anchorY="top" anchorX="left" lineHeight={0.8} position={[-0.375, 0.715, 0.01]} material-toneMapped={false}>
+        {name}
+      </Text>
+      <Text font={suspend(regular).default} fontSize={0.1} anchorX="right" position={[0.4, -0.659, 0.01]} material-toneMapped={false}>
+        /{id}
+      </Text>
+      <Text font={suspend(regular).default} fontSize={0.04} anchorX="right" position={[0.0, -0.677, 0.01]} material-toneMapped={false}>
+        {author}
+      </Text>
+      <mesh name={id} onDoubleClick={(e) => (e.stopPropagation(), setLocation('/item/' + e.object.name))} onPointerOver={(e) => hover(true)} onPointerOut={() => hover(false)}>
+        <roundedPlaneGeometry args={[width, height, 0.1]} />
+        <MeshPortalMaterial ref={portal} events={params?.id === id} side={THREE.DoubleSide}>
+          <color attach="background" args={[bg]} />
+          {children}
+        </MeshPortalMaterial>
+      </mesh>
+    </group>
   )
 }

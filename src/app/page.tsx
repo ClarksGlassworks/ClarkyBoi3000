f88f46d98342320ccdb76db7f97c5c1fc1a72e37
Aaron Taylor <aaron@stayapp.co>
2024-01-10 18:21:02 -0500

    scale down

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,20 +6,41 @@ import { useEffect, useRef, useState } from 'react'
 export default function Home() {
   const [gameboyRotation, setGameboyRotation] = useState(0);
   const [isGameboyRotated, setIsGameboyRotated] = useState(false);
-  const [animationClass, setAnimationClass] = useState('-bottom-[100%]');
+  const [animationClass, setAnimationClass] = useState('scale-50 lg:scale-70');
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  useEffect(() => {
+    setAnimationClass(' scale-50 lg:scale-70');
+    setPosition({
+      x: window.innerWidth, // Subtract half the width of the image
+      y: window.innerHeight, // Subtract half the height of the image
+    });
+  }, []);
 
   useEffect(() => {
-    setAnimationClass('bottom-[10%]');
+    const handleResize = () => {
+      setPosition({
+        x: window.innerWidth, // Subtract half the width of the image
+        y: window.innerHeight, // Subtract half the height of the image
+      });
+    };
+  
+    // Call handleResize right away so the position is set correctly initially
+    handleResize();
+  
+    // Add the event listener
+    window.addEventListener('resize', handleResize);
+  
+    // Remove the event listener when the component unmounts
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   const handleGameboyClick = () => {
     if (isGameboyRotated) {
       setGameboyRotation(0);
-      setAnimationClass('');
+      setAnimationClass(' scale-50 lg:scale-70');
     } else {
       setGameboyRotation(20);
-
-      setAnimationClass('');
+      setAnimationClass(' scale-50 lg:scale-70');
     }
 
 
@@ -29,7 +50,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between">
       <div className="hash-lines min-h-screen w-full h-full z-20 fixed left-0 right-0 top-0 bottom-0"></div>
       <div className="layer1 min-h-screen w-full h-full z-20 fixed left-0 right-0 top-0 bottom-0"></div>
-      <div id="canvas-container" className='w-full h-screen z-30'>
+      <div id="canvas-container" className='w-full h-screen z-30 '>
         <div style={{ width: "100vw", height: "100vh" }}>
 
         </div>
@@ -42,16 +63,17 @@ export default function Home() {
         height={300}
         className='z-30 fixed top-0 right-0'
       />
+      <div className='z-30 fixed bottom-auto lg:bottom-10 top-24 lg:top-auto left-6 lg:left-20 animate-float'>
       <Image
         src="https://wp.clarksglassworks.com/wp-content/uploads/2023/12/clark-tape.png"
         alt="Clark Tape"
         width={500}
         height={300}
-        className='z-30 fixed w-[80%] lg:w-auto top-[100px] lg:top-auto lg:bottom-[50px] lg:left-[90px] animate-float cursor-pointer'
+        className='z-30 cursor-pointer scale-100 lg:scale-75'
       />
+      </div>
 
-      <div onClick={handleGameboyClick} className={`z-30 cursor-pointer fixed  rotate-[${gameboyRotation}deg]  ${animationClass} translate-y-[20%] lg:translate-y-[50px] animated transition-all duration-500 scale-50 lg:scale-100`}>
-       
+      <div onClick={handleGameboyClick} className={`z-30 cursor-pointer fixed   rotate-[${gameboyRotation}deg] ${animationClass} left-[${position.x}px] top-[${position.y}px] animated transition-all duration-500 `}>
        <div className='relative'>
          <Image
           src="https://wp.clarksglassworks.com/wp-content/uploads/2023/12/gameboy-7.png"

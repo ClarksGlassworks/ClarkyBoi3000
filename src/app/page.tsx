030b5390b6ec82bff46e6d6de0b86998eb624959
Aaron Taylor <aaron@stayapp.co>
2024-01-10 19:02:43 -0500

    functional gameboy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,43 +4,35 @@ import Image from 'next/image'
 import { useEffect, useRef, useState } from 'react'
 
 export default function Home() {
-  const [gameboyRotation, setGameboyRotation] = useState(0);
+  const [gameboyRotation, setGameboyRotation] = useState(20);
   const [isGameboyRotated, setIsGameboyRotated] = useState(false);
-  const [animationClass, setAnimationClass] = useState('scale-50 lg:scale-70');
-  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [animationClass, setAnimationClass] = useState('opacity-0');
+  const [position, setPosition] = useState({ x: 2000, y: 2000 });
   useEffect(() => {
-    setAnimationClass(' scale-50 lg:scale-70');
+    setAnimationClass('opacity-100');
     setPosition({
-      x: window.innerWidth, // Subtract half the width of the image
-      y: window.innerHeight, // Subtract half the height of the image
+      x: window.innerWidth - 300, // Subtract half the width of the image
+      y: window.innerHeight - 430, // Subtract half the height of the image
     });
   }, []);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setPosition({
-        x: window.innerWidth, // Subtract half the width of the image
-        y: window.innerHeight, // Subtract half the height of the image
-      });
-    };
   
-    // Call handleResize right away so the position is set correctly initially
-    handleResize();
-  
-    // Add the event listener
-    window.addEventListener('resize', handleResize);
-  
-    // Remove the event listener when the component unmounts
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
 
   const handleGameboyClick = () => {
     if (isGameboyRotated) {
       setGameboyRotation(0);
-      setAnimationClass(' scale-50 lg:scale-70');
+      setAnimationClass('');
+     setPosition({
+  x: window.innerWidth / (window.matchMedia("(max-width: 768px)").matches ? 2 : 1.5) - (window.matchMedia("(max-width: 768px)").matches ? 150 : 300),
+  y: window.innerHeight / (window.matchMedia("(max-width: 768px)").matches ? 2 : 1.5) - (window.matchMedia("(max-width: 768px)").matches ? 215 : 430),
+});
     } else {
       setGameboyRotation(20);
-      setAnimationClass(' scale-50 lg:scale-70');
+      setAnimationClass('');
+      setPosition({
+        x: window.innerWidth - 300, // Subtract half the width of the image
+        y: window.innerHeight - 430, // Subtract half the height of the image
+      });
     }
 
 
@@ -73,17 +65,34 @@ export default function Home() {
       />
       </div>
 
-      <div onClick={handleGameboyClick} className={`z-30 cursor-pointer fixed   rotate-[${gameboyRotation}deg] ${animationClass} left-[${position.x}px] top-[${position.y}px] animated transition-all duration-500 `}>
+
+
+      <div className="absolute w-full h-full flex items-center justify-center cursor-pointer">
+        <div style={{ left: `${position.x}px`, top: `${position.y}px` }} onClick={handleGameboyClick} className={`z-30 fixed rotate-[${gameboyRotation}deg] ${animationClass} transition-all duration-500`}>
+          <Image
+            src="https://wp.clarksglassworks.com/wp-content/uploads/2023/12/gameboy-7.png"
+            alt="Gameboy"
+            width={500}
+            height={300}
+            className='w-[300px]'
+          />
+        </div>
+      </div>
+
+      {/* <div onClick={handleGameboyClick} className={`z-30 cursor-pointer  left-[${position.x}px] top-[${position.y}px] fixed transition-all duration-500 `}>
        <div className='relative'>
          <Image
           src="https://wp.clarksglassworks.com/wp-content/uploads/2023/12/gameboy-7.png"
           alt="Gameboy"
           width={500}
           height={300}
-          className='z-30 relative'
+          className='z-30 relative scale-50'
         />
 
-      <div className='absolute top-10 left-20 z-10 right-10'>
+
+
+
+      <div className='absolute top-10 left-20 z-10 right-10 scale-50'>
         <Image
           src="https://wp.clarksglassworks.com/wp-content/uploads/2024/01/406843489_10224453376305909_4200315121223775507_n-249x300.jpg"
           alt="screen1"
@@ -91,7 +100,7 @@ export default function Home() {
           height={249}
           className='z-10 '
         />
-        {/* <Image
+        <Image
           src="https://wp.clarksglassworks.com/wp-content/uploads/2024/01/406242813_10224453376505914_7619948699842207230_n-226x300.jpg"
           alt="screen1"
           width={300}
@@ -118,10 +127,10 @@ export default function Home() {
           width={300}
           height={249}
           className='z-10 '
-        /> */}
+        />
         </div>
-        </div>
-      </div>
+        </div> 
+      </div>*/}
     </main>
   )
 }

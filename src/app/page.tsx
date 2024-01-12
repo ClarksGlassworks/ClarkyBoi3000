aa24bbe012918bf7d74492df52d0cdbf7966aef8
Aaron Taylor <aaron@stayapp.co>
2024-01-12 12:23:14 -0500

    woot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,19 +39,23 @@ export default function Home() {
   }, [nextImage]);
 
 
-  useEffect(() => {
-     // @ts-ignore
+
+  useEffect(()=>{
+
     const handleClickOutside = (event) => {
       // @ts-ignore
       if (gameboyRef.current && !gameboyRef.current.contains(event.target)) {
         handleGameboyClick();
       }
     };
+    if(isGameboyRotated){
     document.addEventListener('mousedown', handleClickOutside);
+    }
+
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
-  }, []);
+  },[isGameboyRotated])
 
 
   const gameboyImages = [
@@ -162,14 +166,24 @@ export default function Home() {
         />
       </div>
 
-    <div  className="z-30 fixed left-0 top-0">
-      <Image
+    <div  className="z-30 fixed left-[8%] right-[8%] top-[40%] lg:top-10  w-auto  lg:w-[600px] p-2 border-2 border-[#ca6707] bg-white ">
+      <div className="w-full  bg-[#fdd5a8] text-[#ca6707] p-2">
+
+
+      Clark's Top 8 Friends
+
+      </div>
+      <div className="mt-2 grid grid-cols-4 gap-2">
+        <div className="bg-white col-span-1  flex flex-col items-center justify-center "><Image  alt="tom from mypsace" src="https://wp.clarksglassworks.com/wp-content/uploads/2024/01/tom_from_myspace_snl-e1705079509242.jpeg" width={150} height={150} className="max-h-[150px] max-w-[120px]  border-2 object-fill border-[#ca6707] overflow-hidden" /><div className="text-blue-500 text-sm underline mt-2">Tom</div></div>
+        <div className="bg-white col-span-1  flex flex-col items-center justify-center "><Image  alt="tom from mypsace" src="https://wp.clarksglassworks.com/wp-content/uploads/2024/01/Screenshot-2024-01-11-at-7.45.23-AM.png" width={150} height={150} className="max-h-[150px] max-w-[120px]  border-2 object-fill border-[#ca6707] overflow-hidden" /><div className="text-blue-500 text-sm underline mt-2 text-center">WooCommerce Product</div></div>
+        </div>
+      {/* <Image
       src="https://wp.clarksglassworks.com/wp-content/uploads/2024/01/20240108_191900-1.png"
       alt="bong"
       width={500}
       height={300}
       className="z-30 cursor-pointer scale-100 lg:scale-75"
-       />
+       /> */}
     </div>
       <div className="absolute w-full h-full flex items-center justify-center cursor-pointer">
         <div
